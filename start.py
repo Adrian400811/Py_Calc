7f674aa475580b271f320d520bcae698ea0cff5f
HK0811 <59413622+HK0811@users.noreply.github.com>
2020-06-03 23:00:51 +0800

    Update start.py
    Added choose calc methon(+ - * /) and exit if it is not supported.

--- start.py
+++ start.py
@@ -1,5 +1,18 @@
 import time
 
-print('Hi there')
+print("please enter ur name (just hit enter if you don't want to type).")
+name = input()
+print('Hi ' + name +'.')
 time.sleep(2)
 print('Do you want to calculate addition(+) subtraction(-) multiplication(*) or divition(/)?')
+calc = input()
+if calc == ('+'):
+    print('You choosed addition.')
+elif calc == ('-'):
+    print('You choosed subtraction.')
+elif calc == ('*'):
+    print('You choosed multiplication.')
+elif calc == ('/'):
+    print('You choosed divition')
+else:
+    print('This is not a supported calculation methon. Thanks for using our service')
