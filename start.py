087fe30f714ecd84b07919bf0a4103df0a581bc2
HK0811 <59413622+HK0811@users.noreply.github.com>
2020-06-15 10:01:25 +0800

    Update start.py
    “+-*/sq_rt” -> “1234”

--- start.py
+++ start.py
@@ -3,45 +3,51 @@ import numpy
 import math
 
 def calc():
-    print('addition(+) subtraction(-) multiplication(*) divition(/) square_root(sq_rt)?')
-    met = input()
-    if met == ('+'):
+    print('----------------------------------------------------------------------------------------------------')
+    print('1. addition')
+    print('2. subtraction')
+    print('3. multiplication')
+    print('4. divition')
+    print('5. square_root')
+    print('----------------------------------------------------------------------------------------------------')
+    met = input(1/2/3/4)
+    if met == ('1'):
         print('You choosed addition.')
-    elif met == ('-'):
+    elif met == ('2'):
         print('You choosed subtraction.')
-    elif met == ('*'):
+    elif met == ('3'):
         print('You choosed multiplication.')
-    elif met == ('/'):
+    elif met == ('4'):
         print('You choosed divition')
-    elif met == ('sq_rt'):
+    elif met == ('5'):
         print('You choosed square_root')
     else:
         print(f'{met} is not a supported calculation method. Please input a supported method')
         time.sleep(1)
         calc()
     
-    if met == ("+"):
+    if met == ("1"):
         print("Please enter the first number.")
         num1 = input()
         print("Please enter the second number.")
         num2 = input()
         print(f'The answer is {int(num1) + int(num2)}')
         fin()
-    elif met == ("-"):
+    elif met == ("2"):
         print("Please enter the first number.")
         num1 = input()
         print("Please enter the second number.")
         num2 = input()
         print(f'The answer is {int(num1) - int(num2)}')
         fin()
-    elif met == ("*"):
+    elif met == ("3"):
         print("Please enter the first number.")
         num1 = input()
         print("Please enter the second number.")
         num2 = input()
         print(f'The answer is {int(num1) * int(num2)}')
         fin()
-    elif met == ("sq_rt"):
+    elif met == ("5"):
         print('Please enter the number')
         num1 = input()
         print(f'The answer is {math.sqrt(int(num1))}')
